8901dac5a8ecd6cc2b8cb57044e9452b1192cdd3
cameron <cameroncairns@gmail.com>
2016-03-31 19:02:12 -0700

    Adds logic to display from lowest to highest available carbs each food group in the USDA database

--- main.js
+++ main.js
@@ -1,32 +1,78 @@
-$(document).ready(function () {
-    $.getJSON('resources/nutrition-data.json', function(data) {
-        var tableHeader = '';
-        var tableData = '';
-        var dataTable = document.getElementById("data-table");
-        var sort_key = 'carbohydrate' 
-           /* (
-                document.getElementById("sort-key").getAttribute('value') ||
-                'carbohydrate'
-                )
-                */
+function fetchJSON(url, dataStore) {
+    //return $.getJSON(url, function(json_data) {dataStore = json_data});
+    $.ajax({url:url,
+            dataType:'json',
+            async: false,
+            success:function(json_data) {
+                return json_data}});
+}
+
+function generatePage(data, nutrient_data, food_groups) {
+    var tableHeader;
+    var HTML = document.getElementById("javascript-fill-in");
+    var sortKey = 'available_carbohydrate';
+    var attributes = ['description', 'manufacturer',
+                      'available_carbohydrate'];
+    //Sort food_groups into alphabetical order
+    food_groups.sort();
+    // Sort the data according to the defined sortKey
+    // Separate comparison logic for numerical and string attributes
+    if( sortKey in ['description', 'food_group', 'manufacturer']) {
         data.sort( function(a, b) {
-            return a[sort_key] - b[sort_key]
+            return a[sortKey] >= b[sortKey] ? 1 : -1;
         });
-        tableHeader = '<tr>' +
-            '<th>Food</th>' +
-            '<th>Carbohydrates</th>' +
-            '<th>Fiber</th>' +
-            '<th>Available Carbohydrates</th>' +
-            '</tr>';
-        data.forEach( function(itemEntry, itemIndex, itemArray) {
-            //Loop over every fooditem in the nutrition-data file
-            tableData += '<tr>' +
-                '<td>' + itemEntry['description'] + '</td>' +
-                '<td>' + itemEntry['carbohydrate'] + '</td>' +
-                '<td>' + itemEntry['fiber'] + '</td>' +
-                '<td>' + itemEntry['available_carbohydrate'] + '</td>' +
-                '</tr>';
+    } else {
+        data.sort( function(a, b) {
+            return a[sortKey] - b[sortKey];
         });
-        dataTable.innerHTML = tableHeader + tableData;
-    });
+    }
+    // Construct the table header that will be shared amongst each food group
+    // consists of headers defined by the attributes list
+    tableHeader = '<table class="table"><tr>';
+    for (var i = 0; i < attributes.length; i++) {
+        // Display every attribute specified in the attributes list
+        tableHeader +=  '<th>' + attributes[i] + '</th>';
+    }
+    sections = {}
+    for (var i = 0; i < food_groups.length; i++) {
+        sections[food_groups[i]] = '<h3>' + food_groups[i] + '</h3>';
+        sections[food_groups[i]] += tableHeader;
+    }
+    for (var i = 0; i < data.length; i++) {
+        //Loop over every fooditem in the nutrition-data file
+        sections[data[i]['food_group']] += '<tr>';
+        for (var j = 0; j < attributes.length; j++) {
+            // Display every attribute specified in the attributes list
+            sections[data[i]['food_group']] += '<td>' +
+                                                   data[i][attributes[j]] +
+                                               '</td>';
+        }
+        sections[data[i]['food_group']] += '</tr>';
+    }
+    tableFooter = '</table>';
+    pageHTML = '';
+    for (var food_group in sections) {
+        pageHTML += sections[food_group]
+        pageHTML += tableFooter
+    }
+    HTML.innerHTML = pageHTML;
+}
+
+$(document).ready(function () {
+    // Load the necessary JSON data before moving on with the rest of the logic
+    var data, nutrient_data, food_groups
+    //Wait for the all the JSON data to load
+    $.ajax({url:'resources/nutrition-data.json',
+            dataType:'json',
+            async: false,
+            success: function (json_data){data=json_data;}});
+    $.ajax({url:'resources/nutrient-descriptions.json',
+            dataType:'json',
+            async: false,
+            success: function (json_data){nutrient_data=json_data;}});
+    $.ajax({url:'resources/food-groups.json',
+            dataType:'json',
+            async: false,
+            success: function (json_data){food_groups=json_data;}});
+    generatePage(data, nutrient_data, food_groups);
 });
